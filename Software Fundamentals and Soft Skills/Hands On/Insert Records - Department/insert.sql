3c1e513c2a60afd78da9ba81f435d44188ae089b
Chayan Hazra <66939321+yancha38@users.noreply.github.com>
2020-11-01 12:04:28 +0530

    Create insert.sql

---
+++ Software Fundamentals and Soft Skills/Hands On/Insert Records - Department/insert.sql
@@ -0,0 +1,3 @@
+insert into department(Department_id, Department_name, department_block_number) values (1, "CSE", 3);
+insert into department(Department_id, Department_name, department_block_number) values (2, "IT", 3);
+insert into department(Department_id, Department_name, department_block_number) values (3, "SE", 3);
