12edb15bb3f5b7e35f71493ea64126468257a308
Chayan Hazra <66939321+yancha38@users.noreply.github.com>
2021-03-11 23:38:41 +0530

    Add files via upload

---
+++ Java Programming Fundamentals/Shape - Area Volume Calculation/Cube.java
@@ -0,0 +1,36 @@
+public class Cube extends Shape implements Spatial {
+   private double length;
+   private double width;
+   private double height;
+   
+   public double getLength(){
+       return length;
+   }
+   public void setLength(double length){
+       this.length = length;
+   }
+   
+   public double getWidth(){
+       return width;
+   }
+   public void setWidth(double width){
+       this.width = width;
+   }
+   
+   public double getHeight(){
+       return height;
+   }
+   public void setHeight(double height){
+       this.height = height;
+   }
+   
+   @Override
+   public double area(){
+       return ((2 *length * width) + (2 * length * height) + (2 * width * height));
+   }
+   
+   @Override
+   public double volume(){
+       return (length * width * height);
+   }
+}
