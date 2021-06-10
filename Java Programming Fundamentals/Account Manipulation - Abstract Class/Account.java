47f10246ede3e26714dd0e18236deafcdd87b0dc
Chayan Hazra <66939321+yancha38@users.noreply.github.com>
2021-06-10 10:57:17 +0530

    Create Account.java

---
+++ Java Programming Fundamentals/Account Manipulation - Abstract Class/Account.java
@@ -0,0 +1,38 @@
+public abstract class Account {
+    protected int accountNumber;
+    protected Customer customerObj;
+    protected double balance;
+    
+  //Uncomment the getters and setters after writing the attributes
+    public Account(int accountNumber, Customer customerObj, double balance) {
+        this.accountNumber = accountNumber;
+        this.customerObj = customerObj;
+        this.balance = balance;
+    }
+    
+    public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public Customer getCustomerObj() {
+		return customerObj;
+	}
+
+	public void setCustomerObj(Customer customerObj) {
+		this.customerObj = customerObj;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+    
+    public abstract boolean withdraw(double amount);
+}   
